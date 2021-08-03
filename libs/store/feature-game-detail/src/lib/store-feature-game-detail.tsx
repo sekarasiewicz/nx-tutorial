74d5c8dcb94531c18ebfc6b74e999a9d58a4d655
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2021-08-03 21:52:37 +0200

    Details

--- libs/store/feature-game-detail/src/lib/store-feature-game-detail.tsx
+++ libs/store/feature-game-detail/src/lib/store-feature-game-detail.tsx
@@ -1,6 +1,7 @@
 import { RouteComponentProps } from 'react-router-dom'
-import {Card, CardActionArea, Typography} from "@material-ui/core";
-
+import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@material-ui/core';
+import { useEffect, useState } from 'react';
+import { formatRating } from '@nxegghead/store/formatters';
 
 type TParam = {
   id: string
@@ -8,17 +9,68 @@ type TParam = {
 
 export type StoreFeatureGameDetailProps = RouteComponentProps<TParam>
 
-export function StoreFeatureGameDetail(props: StoreFeatureGameDetailProps) {
+export const StoreFeatureGameDetail = (props: StoreFeatureGameDetailProps) => {
   const { match: { params: { id } } } = props
+  const [state, setState] = useState<{
+    data: any,
+    loadingState: 'success' | 'error' | 'loading'
+  }>({
+    data: {},
+    loadingState: 'success'
+  })
+  useEffect(() => {
+    setState({
+      ...state,
+      loadingState: 'loading'
+    })
+    fetch(`/api/games/${id}`)
+      .then((x) => x.json())
+      .then((res) => {
+        setState({
+          ...state,
+          data: res,
+          loadingState: 'success'
+        })
+      }).catch((err) => {
+        setState({
+          ...state,
+          loadingState: 'error'
+        })
+    })
+  }, [id])
+  let info
+  if (state.loadingState === 'loading') {
+    info = 'Loading...'
+  } else if (state.loadingState === 'error'){
+    info = <div>Error fetching data</div>
+  }
+
   return (
     <div className="container">
-      <Card>
-        <CardActionArea>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {id}
-          </Typography>
-        </CardActionArea>
-      </Card>
+      {info ? info :
+        <Card>
+          <CardActionArea>
+            <CardMedia
+              className="game-card-media"
+              image={state.data.image}
+              title={state.data.name}
+            />
+            <CardContent>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {state.data.name}
+              </Typography>
+              <Typography
+                variant="body2"
+                color="textSecondary"
+                component="p"
+                className="game-rating"
+              >
+                <strong>Rating:</strong> {formatRating(state.data.rating)}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      }
     </div>
   );
 }
